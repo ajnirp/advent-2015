82dfd62090cba6e1680bb2d1654cac93e68e5287
rohan.prinja <rohan.prinja@samsung.com>
2015-12-31 18:47:45 +0900

    18

--- 18.py
+++ 18.py
@@ -109,7 +109,7 @@ grid = init_grid(inp)
 def count_live_neighbours(grid, r, c):
 	return sum(grid[y][x] == '#' for x in [c-1,c,c+1] for y in [r-1,r,r+1] if x < len(grid[0]) and x >= 0 and y < len(grid) and y >= 0 and (x != c or y != r))
 
-def step1(grid):
+def step(grid):
 	res = [[cell for cell in row] for row in grid]
 	for row in xrange(len(grid)):
 		for col in xrange(len(grid[row])):
@@ -121,6 +121,21 @@ def step1(grid):
 	return res
 
 for i in xrange(100):
-	grid = step1(grid)
+	grid = step(grid)
 
 print sum(sum(cell == '#' for cell in row) for row in grid) # 821
+
+def set_corners(grid):
+	grid[0][0] = '#'
+	grid[len(grid)-1][0] = '#'
+	grid[0][len(grid[0])-1] = '#'
+	grid[len(grid)-1][len(grid[0])-1] = '#'
+
+grid = init_grid(inp)
+set_corners(grid)
+
+for i in xrange(100):
+	grid = step(grid)
+	set_corners(grid)
+
+print sum(sum(cell == '#' for cell in row) for row in grid) # 886
